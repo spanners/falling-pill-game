97be403ca20d4742c2a2a57051c27e9ff624fbf0
Simon Buist <simon.buist@gmail.com>
2014-03-06 20:42:26 -0500

    Use let expression and forward function application

--- Main.elm
+++ Main.elm
@@ -7,8 +7,8 @@ relativeMouse (ox, oy) (x,y) = (x - ox, -(y - oy))
 center : (Int, Int) -> (Int, Int)
 center (w, h) = (div w 2, div h 2)
 
-render (x, y) = color gray <| collage 400 400 [move (toFloat x, toFloat y) <| filled lightBlue <| circle 15
-                                ]
+render (x, y) = let forms = [circle 15 |> filled lightBlue |> move (toFloat x, toFloat y)]
+                in color gray <| collage 400 400 forms
 
 main : Signal Element
 main = lift render <| relativeMouse (200, 200) <~ Mouse.position
