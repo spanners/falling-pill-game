fa8ca8f1b193bf59cb7604a77c1ff26ba4998876
Simon Buist <simon.buist@gmail.com>
2014-03-06 20:57:00 -0500

    Take the defaultPill attrs as input to formPill

--- Main.elm
+++ Main.elm
@@ -7,10 +7,20 @@ relativeMouse (ox, oy) (x,y) = (x - ox, -(y - oy))
 center : (Int, Int) -> (Int, Int)
 center (w, h) = (div w 2, div h 2)
 
-render (x, y) = let forms = [circle 15 |> filled lightBlue |> move (toFloat x, toFloat y)]
-                in color gray <| collage 400 400 forms
+defaultPill = { pos = (0,0)
+              , vel = (0,-1)
+              , rad = 15
+              , col = lightRed }
+
+
+render (w, h) = 
+    let formPill {rad, col} = circle rad |> filled col
+        forms = [formPill defaultPill] 
+    in color lightGray <| container w h middle 
+                       <| color white
+                       <| collage 400 400 forms
 
 main : Signal Element
-main = lift render <| relativeMouse (200, 200) <~ Mouse.position
+main = lift render Window.dimensions
 
 
