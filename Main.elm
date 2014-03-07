8735d919bb22bd5d9f70f7bd3bd19322d9c96171
Simon Buist <simon.buist@gmail.com>
2014-03-06 21:10:03 -0500

    build a stepping function for each frame of the mainLoop, to update the defaultPill state

--- Main.elm
+++ Main.elm
@@ -7,15 +7,27 @@ relativeMouse (ox, oy) (x,y) = (x - ox, -(y - oy))
 center : (Int, Int) -> (Int, Int)
 center (w, h) = (div w 2, div h 2)
 
+type Vec = (Float, Float)
+
+vecAdd : Vec -> Vec -> Vec
+vecAdd (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+type Pill = {pos:Vec, vel:Vec, rad:Float, col:Color}
+
+
 defaultPill = { pos = (0,0)
               , vel = (0,-1)
               , rad = 15
               , col = lightRed }
 
+stepPill : Pill -> Pill
+stepPill p = { p | pos <- vecAdd p.pos p.vel }
 
 render (w, h) = 
-    let formPill {rad, col} = circle rad |> filled col
-        forms = [formPill defaultPill] 
+    let formPill {rad, col, pos} = 
+                     circle rad |> filled col
+                                |> move pos
+        forms = [formPill <| stepPill defaultPill] 
     in color lightGray <| container w h middle 
                        <| color white
                        <| collage 400 400 forms
